70e0c0118c84514548e0153e9bbf79b3b848ba18
nandobfer <nandobfer@yahoo.com.br>
2023-03-29 19:28:10 -0300

    feat: added mui theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import './sass/App.scss';
 import { Routes, BrowserRouter, Route } from 'react-router-dom';
 import { Login } from './dashboard/pages/Login';
+import { useMuiTheme } from './hooks/useMuiTheme';
+import { ThemeProvider } from '@mui/material';
 
 const App = () => {
+    const muiTheme = useMuiTheme()
+
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route index element={<Login />} />
-                <Route path='/login' element={<Login />} />
-            </Routes>
-        </BrowserRouter>
+        <ThemeProvider theme={muiTheme}>
+
+            <BrowserRouter>
+                <Routes>
+                    <Route index element={<Login />} />
+                    <Route path='/login' element={<Login />} />
+                </Routes>
+            </BrowserRouter>
+
+        </ThemeProvider>
     )
 }
 
---
+++ src/hooks/useMuiTheme.js
@@ -0,0 +1,31 @@
+import { createTheme } from "@mui/material"
+import COLORS from '../sass/_colors.scss'
+
+export const useMuiTheme = () => {
+    const THEME = createTheme({
+        typography: {
+        //  "fontFamily": ["Poppins"].join(','),
+        //  "fontSize": 14,
+        //  "fontWeightLight": 300,
+        //  "fontWeightRegular": 400,
+        //  "fontWeightMedium": 500
+        },
+        palette: {
+            // mode: 'dark',
+
+            primary: {
+                main: COLORS.primary,
+
+            },
+            secondary: {
+                main: COLORS.secondary,
+
+            }, 
+            // success: {
+
+            // }
+        }
+    })
+    
+    return THEME
+}
