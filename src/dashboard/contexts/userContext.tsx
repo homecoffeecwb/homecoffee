a78cdd6cdff39fd8003179d25c6b00e33c45ad1d
nandobfer <nandobfer@yahoo.com.br>
2023-03-30 14:17:15 -0300

    feat: added navigate to login when not user

--- src/dashboard/contexts/userContext.tsx
+++ src/dashboard/contexts/userContext.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { createContext, useState } from 'react';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export interface User {
     name: string
@@ -23,8 +24,11 @@ export default UserContext;
 
 export const UserProvider:React.FC<UserProviderProps> = ({children}) => {
     const [value, setValue] = useState<User | null>(null)
+    const navigate = useNavigate()
 
     useEffect(() => {
+        if (!value) navigate('/dashboard/login')
+        
         console.log(value)
     }, [value])
 
