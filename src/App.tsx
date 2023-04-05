2767a0717e28b173729d42de448c105b3d709778
Dahan Schuster <dan.plschuster@gmail.com>
2023-04-05 17:32:56 -0300

    feat: Format App.tsx and include Menu Main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,29 @@
-import './sass/App.scss';
-import './sass/mui.scss';
-import React from 'react'
-import { Routes, BrowserRouter, Route } from 'react-router-dom';
-import { useMuiTheme } from './hooks/useMuiTheme';
-import { ThemeProvider } from '@mui/material';
-import { Dashboard } from './dashboard/pages/Dashboard';
-import { ProductsProvider } from './common/contexts/productsContext';
+import "./sass/App.scss";
+import "./sass/mui.scss";
+import React from "react";
+import { Routes, BrowserRouter, Route } from "react-router-dom";
+import { useMuiTheme } from "./hooks/useMuiTheme";
+import { ThemeProvider } from "@mui/material";
+import { Dashboard } from "./dashboard/pages/Dashboard";
+import { ProductsProvider } from "./common/contexts/productsContext";
+import Main from "./menu/pages/Main";
 
 const App = () => {
-    const muiTheme = useMuiTheme()
+  const muiTheme = useMuiTheme();
 
-    return (
-        <ThemeProvider theme={muiTheme}>
-            <ProductsProvider>
+  return (
+    <ThemeProvider theme={muiTheme}>
+      <ProductsProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Main />} />
+            <Route path="/dashboard/*" element={<Dashboard />} />
+          </Routes>
+        </BrowserRouter>
+      </ProductsProvider>
+    </ThemeProvider>
+  );
+};
 
-                <BrowserRouter>
-                    <Routes>
-                        <Route index element={<Dashboard />} />
-                        <Route path='/dashboard/*' element={<Dashboard />} />
-                    </Routes>
-                </BrowserRouter>
-                
-            </ProductsProvider>
+export default App;
 
-        </ThemeProvider>
-    )
-}
-
-export default App
