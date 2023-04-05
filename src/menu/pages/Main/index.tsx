90e5dbb4459417ee3ae97221a8d615b8131bb370
Dahan Schuster <dan.plschuster@gmail.com>
2023-04-05 17:32:36 -0300

    feat: Menu main page with the initial Header

---
+++ src/menu/pages/Main/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../../components/Header";
+
+interface MainProps {}
+
+/**
+ * Main page, shows the restaurant menu for the clients
+ */
+const Main: React.FunctionComponent<MainProps> = () => {
+  return (
+    <div>
+				<Header />
+				Alo!
+		</div>
+  );
+};
+
+export default Main;
