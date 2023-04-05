9e53e9d5f8690a8886a66cad5172270d301cc563
Dahan Schuster <dan.plschuster@gmail.com>
2023-04-05 17:43:57 -0300

    feat: Use material buttons in the Header

--- src/menu/components/Header/index.tsx
+++ src/menu/components/Header/index.tsx
@@ -1,24 +1,24 @@
 import React from "react";
-import ShareIcon from '@mui/icons-material/Share';
+import ShareIcon from "@mui/icons-material/Share";
+import IconButton from "@mui/material/IconButton";
 import LogoFlagBranca from "../../../assets/logos/Home's Coffee_COLOR-FLAG.svg";
-import './styles.scss';
+import "./styles.scss";
 
-interface HeaderProps {
-
-}
+interface HeaderProps {}
 
 /**
-* Menu header, used in the restaurant menu page
-*/
+ * Menu header, used in the restaurant menu page
+ */
 const Header: React.FunctionComponent<HeaderProps> = () => {
-
-	return (
-		<div className="MenuHeader">
-			<button className="lang-btn">PT</button>
-			<img src={LogoFlagBranca} className="logo" alt="Home's Coffe" />	
-			<ShareIcon className="share-btn" />
-		</div>
-	);
+  return (
+    <div className="MenuHeader">
+      <IconButton className="lang-btn">PT</IconButton>
+      <img src={LogoFlagBranca} className="logo" alt="Home's Coffe" />
+      <IconButton color="primary" aria-label="Compartilhar">
+        <ShareIcon />
+      </IconButton>
+    </div>
+  );
 };
 
 export default Header;
