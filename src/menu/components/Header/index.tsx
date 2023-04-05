b88a841cceab33655cc2756d9a5e46cda954ac83
Dahan Schuster <dan.plschuster@gmail.com>
2023-04-05 18:10:31 -0300

    feat: Internationalization of the Header
    Use the LanguageContext to render the right text based on the current
    language
    
    Also set the togglePT_EN method as the onClick event of the lang-btn

--- src/menu/components/Header/index.tsx
+++ src/menu/components/Header/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ShareIcon from "@mui/icons-material/Share";
 import IconButton from "@mui/material/IconButton";
+
+import useLanguage from "../../../common/hooks/useLanguage";
+
 import LogoFlagBranca from "../../../assets/logos/Home's Coffee_COLOR-FLAG.svg";
 import "./styles.scss";
 
@@ -10,11 +13,15 @@ interface HeaderProps {}
  * Menu header, used in the restaurant menu page
  */
 const Header: React.FunctionComponent<HeaderProps> = () => {
+  const { language, togglePT_EN } = useLanguage();
+
   return (
     <div className="MenuHeader">
-      <IconButton className="lang-btn">PT</IconButton>
+      <IconButton className="lang-btn" onClick={togglePT_EN}>
+        {language.current}
+      </IconButton>
       <img src={LogoFlagBranca} className="logo" alt="Home's Coffe" />
-      <IconButton color="primary" aria-label="Compartilhar">
+      <IconButton color="primary" aria-label={language.texts.share}>
         <ShareIcon />
       </IconButton>
     </div>
