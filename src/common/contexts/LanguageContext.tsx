6452e09aa632bea142978200f995d03b8d25e6f7
Dahan Schuster <dan.plschuster@gmail.com>
2023-04-05 18:08:57 -0300

    feat: Implement LanguageContext with PT and EN
    Context for holding the JSON object of the currently set language,
    and exposing a method for toggling between PT and EN

---
+++ src/common/contexts/LanguageContext.tsx
@@ -0,0 +1,45 @@
+import React, { createContext, useState, useCallback } from "react";
+
+import { PT, EN } from '../../assets/languages';
+
+interface LanguageObject {
+	current: string;
+	texts: { [Key: string]: string }
+}
+
+interface LanguageContextValues {
+	language: LanguageObject;
+	togglePT_EN: () => void;
+}
+
+const LanguageContext = createContext<LanguageContextValues | null>(null);
+export default LanguageContext;
+
+interface LanguageProviderProps {
+	children: React.ReactNode;
+}
+
+/**
+* Language context for changing the language of the app
+*/
+export const LanguageProvider: React.FC<LanguageProviderProps> = ({ children }) => {
+	const [language, setLanguage] = useState<LanguageObject>(PT);
+
+	const togglePT_EN = useCallback(() => {
+		if (language === PT)
+			setLanguage(EN);
+		else
+			setLanguage(PT);
+	}, [language])
+
+	return (
+		<LanguageContext.Provider
+			value={{
+				language, 
+				togglePT_EN,
+			}}
+		>
+			{children}
+		</LanguageContext.Provider>
+	);
+};
