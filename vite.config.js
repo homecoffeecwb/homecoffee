1373d5003a4dff0858fa3dbe1c582977d175da8e
Dahan Schuster <dan.plschuster@gmail.com>
2023-05-26 10:38:28 -0300

    feat: Arquivo de configuração do Vite

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+
+export default defineConfig(() => {
+  return {
+    build: {
+      outDir: "build",
+    },
+    plugins: [
+      react(),
+      // svgr options: https://react-svgr.com/docs/options/
+      svgr({ svgrOptions: { icon: true } }),
+    ],
+  };
+});
