1caeb0dbd885fde8500f41d62e8cc1b430f85dda
nandobfer <nandobfer@yahoo.com.br>
2023-05-26 16:21:30 -0300

    test: working

--- src/dashboard/pages/ControlPanel/Categories/index.tsx
+++ src/dashboard/pages/ControlPanel/Categories/index.tsx
@@ -7,6 +7,7 @@ import { CategoryContainer } from './CategoryContainer';
 import { SubcategoryContainer } from './SubcategoryContainer';
 import { Button } from '@mui/material';
 import { CategoryModal } from './CategoryModal';
+import { Category } from '../../../../common/contexts/categoriesContext';
 
 interface CategoriesProps {
     
@@ -18,6 +19,11 @@ export const Categories:React.FC<CategoriesProps> = ({  }) => {
     const { categories } = useCategories()
 
     const [newCategoryModal, setNewCategoryModal] = useState(false)
+    const [modalCategory, setModalCategory] = useState<Category|undefined>(undefined)
+
+    useEffect(() => {
+        setNewCategoryModal(true)
+    }, [modalCategory])
 
     useEffect(() => {
         header.setTitle('Categorias')
@@ -27,14 +33,14 @@ export const Categories:React.FC<CategoriesProps> = ({  }) => {
         <div className='Categories-Page' >
             {categories.map(category => 
             <Collapsible key={category.id} 
-                trigger={<CategoryContainer category={category} />}
-                triggerWhenOpen={<CategoryContainer category={category} open />}
+                trigger={<CategoryContainer category={category} modal={setModalCategory} />}
+                triggerWhenOpen={<CategoryContainer category={category} modal={setModalCategory} open />}
                 >
                     {category.subcategories.map(subcategory => <SubcategoryContainer key={subcategory.id} subcategory={subcategory} />)}
                     <Button variant='outlined' >+</Button>
             </Collapsible>)}
             <Button variant='contained' onClick={() => setNewCategoryModal(true)} >nova categoria</Button>
-            <CategoryModal open={newCategoryModal} setOpen={setNewCategoryModal} />
+            <CategoryModal open={newCategoryModal} setOpen={setNewCategoryModal} category={modalCategory} setCategory={setModalCategory} />
         </div>
     )
 }
